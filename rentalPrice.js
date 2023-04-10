80c75fa8f662b68e9959e0c927ac943a04eabbbd
sk0ut <40431259+sk0utxd@users.noreply.github.com>
2023-04-10 13:50:50 +0300

    refactor

--- rentalPrice.js
+++ rentalPrice.js
@@ -1,40 +1,43 @@
-// age - age of driver
-// licence - number of full years person holds driving licence
-// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-// acc - has s/he caused any accidents within last year
-// acc2 - has s/he participated (but not caused) in any accidents within last year
-// season - if it is high season or not
-function price(age, licence, clazz, acc, acc2, season) {
+function calculateRentalPrice(age, licenseYears, carClass, hasRecentAccident, hasParticipatedInAccident, isHighSeason) {
+  const MINIMUM_DRIVING_AGE = 18;
+  const MAX_CLASS_FOR_YOUNG_DRIVERS = 1;
+  const MAX_AGE_FOR_CLASS_1_VEHICLES = 21;
+  const MAX_AGE_FOR_DISCOUNTED_PRICE = 25;
+  const DISCOUNTED_PRICE_MULTIPLIER = 1.3;
+  const RECENT_ACCIDENT_SURCHARGE = 15;
+  const MAX_RENTAL_PRICE = 1000;
 
-    if (age < 18)
-    {
+  if (age < MINIMUM_DRIVING_AGE) {
       return "Driver too young - cannot quote the price";
-    }
-    if (age <= 21 && clazz > 2)
-    {
-        return "Drivers 21 y/o or less can only rent Class 1 vehicles";
-    }
-    var rentalprice = age;
-    if (clazz >= 4 && age <= 25 && season !== false)
-    {
-      rentalprice = rentalprice * 2;
-    }
-    if (licence < 1)
-    {
-        return "Driver must hold driving licence at least for one year. Can not rent a car!";
-    }
-    if (licence < 3)
-    {
-      rentalprice = rentalprice * 1.3;
-    }
-    if (acc == true && age < 30)
-    {
-      rentalprice = rentalprice + 15;
-    }
-    if (rentalprice > 1000)
-    {
-      return 1000.0;
-    }
-    return rentalprice;
+  }
+
+  if (age <= MAX_AGE_FOR_CLASS_1_VEHICLES && carClass > MAX_CLASS_FOR_YOUNG_DRIVERS) {
+      return "Drivers 21 y/o or less can only rent Class 1 vehicles";
+  }
+
+  let rentalPrice = age;
+
+  if (carClass >= 4 && age <= MAX_AGE_FOR_DISCOUNTED_PRICE && isHighSeason) {
+      rentalPrice *= 2;
+  }
+
+  if (licenseYears < 1) {
+      return "Driver must hold driving licence at least for one year. Can not rent a car!";
+  }
+
+  if (licenseYears < 3) {
+      rentalPrice *= DISCOUNTED_PRICE_MULTIPLIER;
+  }
+
+  if (hasRecentAccident && age < 30) {
+      rentalPrice += RECENT_ACCIDENT_SURCHARGE;
+  }
+
+  if (rentalPrice > MAX_RENTAL_PRICE) {
+      return MAX_RENTAL_PRICE;
+  }
+
+  return rentalPrice;
 }
-exports.price = price;
+
+exports.calculateRentalPrice = calculateRentalPrice;
